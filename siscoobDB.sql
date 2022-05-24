e974274abe61e98e112535e96801f7fb3e8a490a
tcayo <tonycayo@hotmail.com>
2022-05-23 20:14:25 -0500

    version 0.02

--- siscoobDB.sql
+++ siscoobDB.sql
@@ -20,6 +20,70 @@ DROP DATABASE IF EXISTS `siscoob`;
 CREATE DATABASE IF NOT EXISTS `siscoob` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 USE `siscoob`;
 
+-- Dumping structure for table siscoob.candidates
+DROP TABLE IF EXISTS `candidates`;
+CREATE TABLE IF NOT EXISTS `candidates` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(120) DEFAULT NULL,
+  `last_name` varchar(120) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table siscoob.candidates: ~2 rows (approximately)
+DELETE FROM `candidates`;
+INSERT INTO `candidates` (`id`, `first_name`, `last_name`) VALUES
+	(1, 'Alejandro', 'Toledo'),
+	(2, 'Pedro Pablo', 'Kuczynski');
+
+-- Dumping structure for table siscoob.candidate_skills
+DROP TABLE IF EXISTS `candidate_skills`;
+CREATE TABLE IF NOT EXISTS `candidate_skills` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `candidate_id` int(11) NOT NULL,
+  `skill_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `candidate_id` (`candidate_id`),
+  KEY `skill_id` (`skill_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table siscoob.candidate_skills: ~5 rows (approximately)
+DELETE FROM `candidate_skills`;
+INSERT INTO `candidate_skills` (`id`, `candidate_id`, `skill_id`) VALUES
+	(1, 1, 1),
+	(2, 1, 2),
+	(3, 1, 3),
+	(4, 2, 1),
+	(5, 2, 2),
+	(6, 2, 3);
+
+-- Dumping structure for procedure siscoob.get_candidate_skill
+DROP PROCEDURE IF EXISTS `get_candidate_skill`;
+DELIMITER //
+CREATE PROCEDURE `get_candidate_skill`(IN candidate_id INT)
+BEGIN
+	SELECT candidates.id, first_name,last_name, skills.name AS skill 
+	FROM candidates
+	INNER JOIN candidate_skills ON candidates.id = candidate_skills.candidate_id
+	INNER JOIN skills ON skills.id = candidate_skills.skill_id
+	WHERE candidates.id = candidate_id;
+    END//
+DELIMITER ;
+
+-- Dumping structure for table siscoob.skills
+DROP TABLE IF EXISTS `skills`;
+CREATE TABLE IF NOT EXISTS `skills` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table siscoob.skills: ~3 rows (approximately)
+DELETE FROM `skills`;
+INSERT INTO `skills` (`id`, `name`) VALUES
+	(1, 'corrupto'),
+	(2, 'ladrón'),
+	(3, 'vende patria');
+
 -- Dumping structure for table siscoob.tblequipos
 DROP TABLE IF EXISTS `tblequipos`;
 CREATE TABLE IF NOT EXISTS `tblequipos` (
@@ -155,8 +219,8 @@ INSERT INTO `tblrol` (`idrol`, `rol`) VALUES
 DROP TABLE IF EXISTS `tbluser`;
 CREATE TABLE IF NOT EXISTS `tbluser` (
   `iduser` int(11) NOT NULL AUTO_INCREMENT,
-  `usuario` varchar(45) NOT NULL,
-  `pass` varchar(45) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `pass` varchar(50) NOT NULL,
   `idpersona` int(11) NOT NULL,
   PRIMARY KEY (`iduser`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
@@ -171,20 +235,29 @@ INSERT INTO `tbluser` (`iduser`, `usuario`, `pass`, `idpersona`) VALUES
 DROP TABLE IF EXISTS `usuarios`;
 CREATE TABLE IF NOT EXISTS `usuarios` (
   `idusuario` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(64) NOT NULL,
   `tipo` int(11) NOT NULL DEFAULT 1,
   `last_session` datetime DEFAULT '1970-01-02 00:00:00',
   `fecha_creacion` datetime DEFAULT current_timestamp(),
   PRIMARY KEY (`idusuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
 
 -- Dumping data for table siscoob.usuarios: ~2 rows (approximately)
 DELETE FROM `usuarios`;
 INSERT INTO `usuarios` (`idusuario`, `username`, `password`, `tipo`, `last_session`, `fecha_creacion`) VALUES
-	(1, 'soporte', 'soporte', 1, '2022-05-05 17:30:35', '2022-05-05 17:30:35'),
-	(2, 'admin', 'admin', 2, '2021-10-11 21:56:25', '2021-07-21 18:48:08'),
-	(4, 'tcayo', '123', 4, '2022-05-05 16:19:57', '2022-05-05 16:19:57');
+	(1, 'soporte', '63F5A3F6CF9822AFB4B0E515D4AA0FF660498061561E8184C89F40D5540A651F', 1, '2022-05-07 10:56:24', '2022-05-07 10:56:24'),
+	(2, 'admin', '008FEEC4C7FE315BFCE5470DBC72DA74C8FFE58B4100AA6D1EC7C952D5CECA8B', 2, '2022-05-07 10:56:15', '2022-05-07 10:56:15'),
+	(7, 'csanchez', 'FFA2ECB6916CDD1603B280AE32F44AF33BEF462BF853A484D8554E3341D5D81B', 1, '2022-05-07 15:47:06', '2022-05-07 15:47:06');
+
+-- Dumping structure for procedure siscoob.usuarios_get
+DROP PROCEDURE IF EXISTS `usuarios_get`;
+DELIMITER //
+CREATE PROCEDURE `usuarios_get`()
+BEGIN
+SELECT * FROM usuarios;
+END//
+DELIMITER ;
 
 -- Dumping structure for table siscoob.usuarios_info
 DROP TABLE IF EXISTS `usuarios_info`;
@@ -203,6 +276,59 @@ DELETE FROM `usuarios_info`;
 INSERT INTO `usuarios_info` (`idusuarios_info`, `nombre`, `apellido`, `idusuario`) VALUES
 	(1, 'Julio', 'Cervera', 1),
 	(2, 'Administrador', 'Admin', 2);
+
+-- Dumping structure for procedure siscoob.usuario_del
+DROP PROCEDURE IF EXISTS `usuario_del`;
+DELIMITER //
+CREATE PROCEDURE `usuario_del`(
+	IN `usuario_id` INT
+)
+BEGIN
+delete from usuarios WHERE idusuario=usuario_id;
+END//
+DELIMITER ;
+
+-- Dumping structure for procedure siscoob.usuario_get
+DROP PROCEDURE IF EXISTS `usuario_get`;
+DELIMITER //
+CREATE PROCEDURE `usuario_get`(
+	IN `usuario_id` INT
+)
+BEGIN
+select * from usuarios WHERE idusuario=usuario_id;
+END//
+DELIMITER ;
+
+-- Dumping structure for procedure siscoob.usuario_new
+DROP PROCEDURE IF EXISTS `usuario_new`;
+DELIMITER //
+CREATE PROCEDURE `usuario_new`(
+	IN `p1` INT,
+	IN `p2` TEXT,
+	IN `p3` TEXT,
+	IN `p4` INT,
+	IN `p5` TIMESTAMP,
+	IN `p6` TIMESTAMP
+)
+BEGIN
+INSERT INTO usuarios VALUES(p1,p2,p3,p4,p5,p6);
+END//
+DELIMITER ;
+
+-- Dumping structure for procedure siscoob.usuario_upd
+DROP PROCEDURE IF EXISTS `usuario_upd`;
+DELIMITER //
+CREATE PROCEDURE `usuario_upd`(
+	IN `p1` TEXT,
+	IN `p2` INT,
+	IN `p3` TIMESTAMP,
+	IN `p4` TIMESTAMP,
+	IN `usuario_id` INT
+)
+BEGIN
+UPDATE usuarios SET PASSWORD=p1,tipo=p2,last_session=p3,fecha_creacion=p4 WHERE idusuario=usuario_id;
+END//
+DELIMITER ;
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
